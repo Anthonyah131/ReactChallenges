99dd366a064937c25b6dbcc5fca95bfcf1e64f29
Anthony Avila <105742243+Anthonyah131@users.noreply.github.com>
2024-03-18 18:15:46 -0600

    Challenges

---
+++ challenge/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import TeamsName from "./components/TeamsName";
+import Animals from "./components/Animals";
+import ShowHideElem from "./components/ShowHideElem";
+import DataBinding from "./components/DataBinding";
+import ParentState from "./components/ParentState";
+import FetchDataAPI from "./components/FetchDataAPI";
+
+function App() {
+
+  return (
+    <>
+      <TeamsName />
+      <Animals/>
+      <ShowHideElem />
+      <DataBinding />
+      <ParentState />
+      <FetchDataAPI />
+    </>
+  )
+}
+
+export default App
---
+++ challenge/src/components/ChildComponent.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    updateText: () => void;
+}
+
+const ChildComponent: React.FC<Props> = ({ updateText }) => {
+    return (
+        <button onClick={updateText} className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
+            Update Text
+        </button>
+    );
+};
+
+export default ChildComponent;
